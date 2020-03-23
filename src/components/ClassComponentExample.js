80e1e69fef34577457a284180728b58376b0ffb3
Rkiguel Figueiro <riguel.figueiro@thoughtworks.com>
2020-03-23 11:48:24 -0300

    added dev configurations

--- src/components/ClassComponentExample.js
+++ src/components/ClassComponentExample.js
@@ -11,83 +11,83 @@ import "../App.css";
 // when you are using eslint please add the plugin eslint-plugin-react-hooks how dev dependency
 class ClassComponentExample extends Component {
 
-    state = {
-        name: '',
-        email: ''
-    }
+  state = {
+    name: '',
+    email: ''
+  }
 
-    handleSubmit = (e) => {
-        debugger
-        e.preventDefault();
-    }
+  handleSubmit = (e) => {
+    debugger
+    e.preventDefault();
+  }
 
-    handleStateValue = (e) => {
-        const fieldName = e.target.name;
-        const value = e.target.value;
-        this.setState({ ...this.state, [fieldName]: value });
-    }
+  handleStateValue = (e) => {
+    const fieldName = e.target.name;
+    const value = e.target.value;
+    this.setState({ ...this.state, [fieldName]: value });
+  }
 
-    handleTextTitle = (e) => {
-        document.title = this.state.name;
-    }
+  handleTextTitle = (e) => {
+    document.title = this.state.name;
+  }
 
-    componentDidMount () {
-        document
-            .querySelector("button")
-            .addEventListener("click", this.handleTextTitle);
+  componentDidMount () {
+    document
+      .querySelector("button")
+      .addEventListener("click", this.handleTextTitle);
 
-        const user = fetchFake('/user/someUser');
-        this.setState(user);
-    }
+    const user = fetchFake('/user/someUser');
+    this.setState(user);
+  }
 
-    componentDidUpdate (prevProps, prevState) {
-        if (prevState.name !== this.state.name
-            || prevState.email !== this.state.email) {
-            localStorage.setItem('workshop-value', this.state)
-        }
+  componentDidUpdate (prevProps, prevState) {
+    if (prevState.name !== this.state.name
+      || prevState.email !== this.state.email) {
+      localStorage.setItem('workshop-value', this.state)
     }
+  }
 
-    componentWillUnmount () {
-        document
-            .querySelector("button")
-            .removeEventListener("click", this.handleTextTitle);
-    }
+  componentWillUnmount () {
+    document
+      .querySelector("button")
+      .removeEventListener("click", this.handleTextTitle);
+  }
 
-    render () {
-        return (
-            <>
-                <form onSubmit={e => this.handleSubmit(e)}>
-                    <img
-                        src="https://i.morioh.com/2934a8d84c.png"
-                        height="150"
-                        width="175"
-                        alt="Unform"
-                    />
-                    <h1>React & Redux Hooks</h1>
-                    <input
-                        name="name"
-                        label="Full name"
-                        placeholder="Name"
-                        onChange={(e) => this.handleStateValue(e)}
-                    />
-                    <input
-                        name="email"
-                        label="E-mail"
-                        type="email"
-                        placeholder="Email"
-                        onChange={(e) => this.handleStateValue(e)}
-                    />
-                    <button type="submit">Enviar</button>
-                </form>
-                <span>State value</span>
-                <pre>{JSON.stringify(this.state, undefined, 2)}</pre>
-            </>
-        )
-    }
+  render () {
+    return (
+      <>
+        <form onSubmit={e => this.handleSubmit(e)}>
+          <img
+            src="https://i.morioh.com/2934a8d84c.png"
+            height="150"
+            width="175"
+            alt="Unform"
+          />
+          <h1>React & Redux Hooks</h1>
+          <input
+            name="name"
+            label="Full name"
+            placeholder="Name"
+            onChange={(e) => this.handleStateValue(e)}
+          />
+          <input
+            name="email"
+            label="E-mail"
+            type="email"
+            placeholder="Email"
+            onChange={(e) => this.handleStateValue(e)}
+          />
+          <button type="submit">Enviar</button>
+        </form>
+        <span>State value</span>
+        <pre>{JSON.stringify(this.state, undefined, 2)}</pre>
+      </>
+    )
+  }
 }
 
 const mapStateToProps = state => ({
-    user: state.user
+  user: state.user
 });
 
 const mapDispatchToProps = dispatch => bindActionCreators({ addUser }, dispatch);
