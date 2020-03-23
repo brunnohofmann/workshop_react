7f968d94513e7aad98b84fbf89820a4afeaed63e
Rkiguel Figueiro <riguel.figueiro@thoughtworks.com>
2020-03-23 10:01:41 -0300

    added componentDidMount and ummount

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React, { Component } from "react";
 import { fetchFake } from './fetchFake'
 import "./App.css";
 
+// Please if you havy someone question about rules visit https://reactjs.org/docs/hooks-rules.html
+
 // when you are using eslint please add the plugin eslint-plugin-react-hooks how dev dependency
 class App extends Component {
 
   state = {
-    name: 'ThoughWorker member',
-    email: 'member@thoughtworks.com'
+    name: '',
+    email: ''
   }
 
-  handleSubmit = () => { }
+  handleSubmit = (e) => {
+    debugger
+    e.preventDefault();
+  }
 
   handleStateValue = (e) => {
     const fieldName = e.target.name;
@@ -20,7 +25,15 @@ class App extends Component {
 
   }
 
+  handleTextTitle = (e) => {
+    document.title = this.state.name;
+  }
+
   componentDidMount () {
+    document
+      .querySelector("button")
+      .addEventListener("click", this.handleTextTitle);
+
     const user = fetchFake('/user/someUser');
     this.setState(user);
   }
@@ -32,33 +45,41 @@ class App extends Component {
     }
   }
 
-  componentWillUnmount () { }
+  componentWillUnmount () {
+    document
+      .querySelector("button")
+      .removeEventListener("click", this.handleTextTitle);
+  }
 
   render () {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <img
-          src="https://i.morioh.com/2934a8d84c.png"
-          height="150"
-          width="175"
-          alt="Unform"
-        />
-        <h1>React & Redux Hooks</h1>
-        <input
-          name="name"
-          label="Full name"
-          placeholder="Name"
-          onChange={(e) => this.handleStateValue(e)}
-        />
-        <input
-          name="email"
-          label="E-mail"
-          type="email"
-          placeholder="Email"
-          onChange={(e) => this.handleStateValue(e)}
-        />
-        <button type="submit">Enviar</button>
-      </form>
+      <>
+        <form onSubmit={e => this.handleSubmit(e)}>
+          <img
+            src="https://i.morioh.com/2934a8d84c.png"
+            height="150"
+            width="175"
+            alt="Unform"
+          />
+          <h1>React & Redux Hooks</h1>
+          <input
+            name="name"
+            label="Full name"
+            placeholder="Name"
+            onChange={(e) => this.handleStateValue(e)}
+          />
+          <input
+            name="email"
+            label="E-mail"
+            type="email"
+            placeholder="Email"
+            onChange={(e) => this.handleStateValue(e)}
+          />
+          <button type="submit">Enviar</button>
+        </form>
+        <span>State value</span>
+        <pre>{JSON.stringify(this.state, undefined, 2)}</pre>
+      </>
     )
   }
 }
